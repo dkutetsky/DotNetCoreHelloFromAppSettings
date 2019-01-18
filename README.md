b1098be94a291f9d519929dbb3e2fa889a92e9ba
Dmitry Kutetsky <dmitry.kutetsky@evolute.com>
2019-01-18 15:37:02 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # DotNetCoreHelloFromAppSettings
-Simple .net core console app that writes value from appsettings.json to the output
+Simple .net core console app that writes value from appsettings.json to the output.
+
+## Solution structure
+
+### /src
+Contains the source code.
+
+### /releases
+Contains all releases.
+
+## How to run
+1. cd to /releases/1.1
+2. execute
+``` shell
+dotnet DotNetCoreHelloFromAppSettings.dll
+```
+
---
+++ src/DotNetCoreHelloFromAppSettings/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace DotNetCoreHelloFromAppSettings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            var configuration = builder.Build();
+
+            var hello = configuration.GetSection("HelloValue").Value;
+            Console.WriteLine(hello);
+            Console.ReadLine();
+        }
+    }
+}
