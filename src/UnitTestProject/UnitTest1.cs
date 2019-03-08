6e8792fe1d93fbb17b302d9199afee9ea41fe8ba
Dmitry Kutetsky <dmitry.kutetsky@evolute.com>
2019-03-08 22:42:10 +0200

    Added project with unit-tests.

---
+++ src/UnitTestProject/UnitTest1.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
